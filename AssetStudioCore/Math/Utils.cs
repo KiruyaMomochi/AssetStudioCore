e9bb51adc6c3466743811a6fafd9ffc2da7ed267
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2020-11-25 03:23:25 +0800

    Use .NET 5.0 built-in half

---
+++ AssetStudioCore/Math/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetStudio
+{
+    internal static class Utils
+    {
+        // Converts an array of bytes into a half.
+        public static Half ToHalf(byte[] value, int startIndex) => Int16BitsToHalf(System.BitConverter.ToInt16(value, startIndex));
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe Half Int16BitsToHalf(short value)
+        {
+            return *(Half*)&value;
+        }
+    }
+}
