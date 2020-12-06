0597de217ee204305bd67ddc8f915d69e0c44001
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2020-12-06 15:52:15 +0800

    Add PowerShell module

---
+++ AssetStudioModule/LoadFiles.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Management.Automation;
+using AssetStudio;
+
+namespace AssetStudioModule
+{
+    [Cmdlet(VerbsData.Import, "UnityAsset")]
+    public class LoadFiles : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string[] Files { get; set; } = Array.Empty<string>();
+
+        protected override void ProcessRecord()
+        {
+            Directory.SetCurrentDirectory(this.SessionState.Path.CurrentFileSystemLocation.Path);
+
+            var assetManager = new AssetStudio.AssetsManager();
+            assetManager.LoadFiles(Files);
+            WriteObject(assetManager);
+            base.ProcessRecord();
+        }
+    }
+
+    [Cmdlet(VerbsCommon.Get, "Texture2D")]
+    public class ReadTexture2D : PSCmdlet
+    {
+        
+        [Parameter(Position = 0, Mandatory = true)]
+        public Texture2D File { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(File.ConvertToBitmap(true));
+        }
+    }
+}
---
+++ AssetStudioUtility/T2DDll.cs
@@ -0,0 +1,8 @@
+namespace Texture2DDecoder
+{
+    internal static class T2DDll
+    {
+
+        internal const string DllName = "Texture2DDecoder";
+    }
+}
